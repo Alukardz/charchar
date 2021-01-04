fa285f6631103b74f05d26ec896b35356ef08331
bdelarosa <bladegt@msn.com>
2021-01-04 10:01:24 -0400

    final

--- char_char.py
+++ char_char.py
@@ -1,3 +1,9 @@
+import sys
+import os
+import tty
+import termios
+
+
 def main():
     header = 'Nombres,Apellidos,Edad,Ahorros,Password'
     doc_dir = os.path.join(os.getcwd(), sys.argv[1])
@@ -11,12 +17,33 @@ def main():
             lines = doc.readlines()
 
             while counter == 0:
-                names = input('Cedula: ')
-                lastnames = input('Nombre: ')
-                age = input('Edad: ')
-                savings = input('Apellido: ')
-                password = input('Apellido: ')
+                pass_counter = 0
+                print('Nombres: ')
+                names = read_char()
+
+                print('Apellidos: ')
+                lastnames = read_char()
+
+                print('Edad: ')
+                age = read_int()
+
+                print('Ahorros: ')
+                savings = read_decimal()
+
+                while pass_counter == 0:
+                    print('Contraseña: ')
+                    password = read_pass()
+
+                    print('Confirmar contraseña: ')
+                    password2 = read_pass()
+
+                    if password == password2:
+                        pass_counter += 1
+                    else:
+                        print('Contraseñas no concuerdan, intenta nuevamente.')
+
                 data = f'{names},{lastnames},{age},{savings},{password}'
+                print(data)
 
                 respuesta = input('¿Deseas guardar? si/no\n')
 
@@ -28,11 +55,103 @@ def main():
                     doc.write('\n')
                     doc.write(data)
                     print('Registro creado.')
+                    counter += 1
+
     except IOError:
         print('Ruta del documento inválida.')
+
+
+def _getch():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+
+    try:
+        tty.setraw(fd)
+        ch = sys.stdin.read(1).strip('\n')
+
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+
+
+def read_char():
+    stringer = ''
+    while True:
+        cha = _getch()
+
+        if cha.isalpha():
+            print(cha, end='', flush=True)
+            stringer += cha
+
+        elif cha == '\x7f':
+            print('\b \b', end='', flush=True)
+
+        elif cha == '\r':
+            print('\r')
+            break
+
+    return stringer
+
+
+def read_int():
+    inger = ''
+    while True:
+        cha = _getch()
+
+        if cha.isdigit():
+            print(cha, end='', flush=True)
+            inger += cha
+
+        elif cha == '\x7f':
+            print('\b \b', end='', flush=True)
+
+        elif cha == '\r':
+            print('\r')
+            break
+
+    return inger
+
+
+def read_decimal():
+    inger = ''
+    while True:
+        cha = _getch()
+
+        if cha.isdigit() or cha == '.' or cha == ',':
+            print(cha, end='', flush=True)
+            inger += cha
+
+        elif cha == '\x7f':
+            print('\b \b', end='', flush=True)
+
+        elif cha == '\r':
+            print('\r')
+            break
+
+    rounded = "%.2f" % float(inger)
+    return rounded
+
+
+def read_pass():
+    passger = ''
+    while True:
+        cha = _getch()
+
+        if cha.isprintable():
+            print('*', end='', flush=True)
+            passger += cha
+
+        elif cha == '\x7f':
+            print('\b \b', end='', flush=True)
+
+        elif cha == '\r':
+            print('\r')
+            break
+
+    return passger
 
 
 if len(sys.argv) >= 2:
     main()
 else:
-    print('Especificar document. ej. reg_data.py datos.csv')
+    print('Especificar documento. ej. reg_data.py datos.csv')
